29d6b5ac382cf4916ea3372396346a1003223244
silviasrl <82866884+silviasrl@users.noreply.github.com>
2022-06-30 08:03:00 +0700

    Add files via upload

--- sippktp.sql
+++ sippktp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2022 at 07:30 PM
+-- Generation Time: Jun 30, 2022 at 03:01 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -80,7 +80,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2022_05_18_043408_create_news_table', 1),
 (6, '2022_05_18_043614_create_registers_table', 1),
 (7, '2022_05_18_085114_create_conditions_table', 1),
-(8, '2022_05_22_032058_add_reason_to_registers_table', 1);
+(8, '2022_06_29_034001_create_notifications_table', 1);
 
 -- --------------------------------------------------------
 
@@ -90,6 +90,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `news` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `source` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `thumbnail` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -102,11 +103,34 @@ CREATE TABLE `news` (
 -- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`id`, `title`, `body`, `thumbnail`, `created_by`, `created_at`, `updated_at`) VALUES
-(1, 'Kabupaten Samosir', 'Samosir Island', 'news/dVVLN2nAjiAoJfLBRkeolU136AG1nZwfSAW0iMYL.jpg', 1, '2022-06-14 23:14:37', '2022-06-24 09:15:28'),
-(2, 'Heboh Wakapolres Samosir Marahi Pastor, Berujung Restorative Justice', 'Wakapolres Samosir Kompol Tongap M Lumban Tobing, terlibat perselisihan dengan Pastor bernama Sabat Nababan. Peristiwa tersebut terjadi di Desa Wisata Tomok, Kecamatan Simanindo, Kabupaten Samosir, Sumatera Utara, Kamis (16/6).', 'news/wxNmCOqFb0tPynN0i4cdUa2sr2GYAGzbU9MJn5YP.jpg', 1, '2022-06-17 08:04:30', '2022-06-23 10:27:58'),
-(3, 'Main-mainlah ke Bukit Holbung Samosir Lokasi Film Ngeri-ngeri Sedap', 'Film Ngeri-ngeri Sedap yang disutradarai dan ditulis oleh Bene Dion Rajagukguk berhasil menyita hati para penonton tanah air, khususnya yang berasal dari Sumatra Utara. Salah satu lokasi syuting film ini di Bukit Holbung, Samosir. Seperti apa keistimewaan Bukit Holbung tersebut?', 'news/oIBJLYPWwmhkpqyrPKsSNix5GfbUC96rzeEA2IPk.jpg', 1, '2022-06-17 08:06:45', '2022-06-23 10:29:19'),
-(4, 'Pemkab Samosir Ikuti Penilaian Verifikasi Lapangan Hybrid (VLH) Evaluasi Kabupaten Layak Anak Tahun 2022', 'Pemerintah Kabupaten Samosir mengikuti penilaian Verifikasi Lapangan Hybrid (VLH) evaluasi Kabupaten Layak Anak (KLA) Tahun 2022 yang diselenggarakan secara hybrid (virtual dan tatap muka) di Aula Kantor dan Lobi lt. 2 Kantor Kantor Bupati Samosir, Pangururan, Jumat (17/6).', 'news/KHFkl3OGeZRcJTOrdgxVA1jOPXkKYPMxNZyxnrNH.jpg', 1, '2022-06-17 08:07:03', '2022-06-23 10:31:14');
+INSERT INTO `news` (`id`, `source`, `title`, `body`, `thumbnail`, `created_by`, `created_at`, `updated_at`) VALUES
+(1, '-', 'Kabupaten Samosir', 'Samosir Island', '1656549999.png', 1, '2022-06-29 17:45:29', '2022-06-29 17:46:39'),
+(2, 'https://sumut.wahananews.co/serbaserbi/ktp-siap-dukung-kemajuan-daerah-samosir-dan-tingkatkan-ekonomi-kreatif-masyarakat-laM7fks5sg', 'KTP Siap Dukung Kemajuan Daerah Samosir dan Tingkatkan Ekonomi Kreatif Masyarakat', 'Kemajuan disuatu daerah akan cepat terlaksana bila masyarakat dan pemerintah daerah satu hati dalam menjalankan program-program yang akan dilaksankan di daerah tersebut. Pembangunan dan program yang akan dikerjakan dapat bermanfaat dan dirasakan masyarakat.', '1656550054.jpg', 1, '2022-06-29 17:45:29', '2022-06-29 17:47:34'),
+(3, 'https://medan.tribunnews.com/2020/11/17/disdukcapil-samosir-jemput-bola-ke-desa-desa-dan-sekolah-untuk-pengurusan-ktp', 'Disdukcapil Samosir Jemput Bola ke Desa-desa dan Sekolah Untuk Pengurusan KTP', 'Dinas Kependudukan dan Pencatatan Sipil (Disdukcapil) Kabupaten Samosir menerapkan metode jemput bola untuk pengurusan Kartu Tanda Penduduk (KTP).', '1656550096.jpg', 1, '2022-06-29 17:45:29', '2022-06-29 17:48:16'),
+(4, 'https://sumut.antaranews.com/berita/181000/perekaman-e-ktp-di-samosir-85-persen-lebih', 'Perekaman e-KTP di Samosir 85 persen lebih', 'Samosir (Antaranews Sumut) - Dalam hal perekaman kartu tanda penduduk secara elektronik (e-KTP) berdasarkan data di Pemprov Sumut, Kabupaten Samosir mencapai di atas 85 persen.', '1656550121.jpg', 1, '2022-06-29 17:45:29', '2022-06-29 17:48:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `message` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `read` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `user_id`, `message`, `read`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Permohonan Baru Dari Daniel. Mohon segera di proses.', 1, '2022-06-29 17:51:20', '2022-06-29 17:52:37'),
+(2, 2, 'Permohonan Anda Telah Diterima. Silahkan cek email anda untuk informasi lebih lanjut.', 1, '2022-06-29 17:53:09', '2022-06-29 17:53:40');
 
 -- --------------------------------------------------------
 
@@ -146,14 +170,17 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `registers` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `nik` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
   `kk` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `akta` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `st` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `reason` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_by` int(11) NOT NULL,
+  `reason` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tanggal_diambil` date DEFAULT NULL,
+  `jam_diambil` time DEFAULT NULL,
+  `lokasi` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `verified_by` int(11) NOT NULL,
   `verified_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -164,8 +191,8 @@ CREATE TABLE `registers` (
 -- Dumping data for table `registers`
 --
 
-INSERT INTO `registers` (`id`, `nik`, `name`, `phone`, `kk`, `akta`, `st`, `reason`, `created_by`, `verified_by`, `verified_at`, `created_at`, `updated_at`) VALUES
-(1, '1212015701020002', 'Silvia Sari', '081262264055', 'register/zxDUxpECXpmOZXcjavnAxED91HkwCZ4O20fQQIGo.jpg', 'register/5FmLId7jW8aju8zc4TPelUbAGyq7LfAZ6TCz8YTB.png', 'permohonan selesai', '', 2, 1, NULL, '2022-06-23 10:53:47', '2022-06-23 10:57:47');
+INSERT INTO `registers` (`id`, `user_id`, `nik`, `name`, `phone`, `kk`, `akta`, `st`, `reason`, `tanggal_diambil`, `jam_diambil`, `lokasi`, `verified_by`, `verified_at`, `created_at`, `updated_at`) VALUES
+(1, 2, '12121212311344', 'Daniel', '087612523241', 'register/bd0Od5OeKAWlNHNOYrjaS3MBwNRP3wODG0Ue9WZV.jpg', 'register/qGcpbNJdtyGti5Itk75trUx31zz7rKnr2eJQQ7mb.jpg', 'diterima', NULL, '2022-07-02', '11:55:00', 'Dukcapil Samosir', 1, NULL, '2022-06-29 17:51:20', '2022-06-29 17:53:09');
 
 -- --------------------------------------------------------
 
@@ -190,12 +217,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$z0XMEW8ySnzdJg.HWTdlXe5D2JetRBFXtqPbnkletzH3nGAyVp38G', 'a', NULL, '2022-06-06 23:34:21', '2022-06-06 23:34:21'),
-(2, 'Silvia', 'silvialubis14@gmail.com', NULL, '$2y$10$hh7Xu6QxT8ZeDi16Y1fS3O7FuKjVHDPH2JyOzOjqy94QAGS593H9y', 'u', NULL, '2022-06-06 23:35:15', '2022-06-06 23:35:15'),
-(3, 'Rosani', 'rosani@gmail.com', NULL, '$2y$10$c1fBBT5Q9W5kSv1WiE3e8.OVrWRrxT1EYUZSBPSdpfvwTpbVNPT4i', 'u', NULL, '2022-06-22 20:52:57', '2022-06-22 20:52:57'),
-(4, 'Easter', 'easter@gmail.com', NULL, '$2y$10$lRWa77h.9e5FXb.1p5k99uOk5XntNfeLVB3r01g8Z6z4P6HT73C6y', 'u', NULL, '2022-06-22 20:54:45', '2022-06-22 20:54:45'),
-(5, 'Laksamana', 'laksa@gmail.com', NULL, '$2y$10$qL/WPGk9o.1hkavqgBl2VelbKt2c8gBQFC2wIViVDQFXGmSS1vQfe', 'u', NULL, '2022-06-22 20:55:55', '2022-06-22 20:55:55'),
-(6, 'Simon', 'simon@gmail.com', NULL, '$2y$10$tZ51xablDK5zdCCKQFVC6.0V2yvr3ploDfpdgCzK6nQe0EEb447H2', 'u', NULL, '2022-06-22 20:56:48', '2022-06-22 20:56:48');
+(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$PUw03enlLFhx4XUVmRgV0OR/b6VrWWvhIJtjm8a1ikyq6ug2BkJY6', 'a', NULL, '2022-06-29 17:45:29', '2022-06-29 17:45:29'),
+(2, 'Dania', 'lubissilvia742@gmail.com', NULL, '$2y$10$v0LM2ejx6VaiE5ukLkJxoul5NjcRZDJ8IdlVWSSjtz5W0L9A9o0UG', 'u', NULL, '2022-06-29 17:49:26', '2022-06-29 17:49:26');
 
 --
 -- Indexes for dumped tables
@@ -227,6 +250,13 @@ ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notifications_user_id_foreign` (`user_id`);
+
+--
 -- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -244,7 +274,8 @@ ALTER TABLE `personal_access_tokens`
 -- Indexes for table `registers`
 --
 ALTER TABLE `registers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `registers_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -261,7 +292,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `conditions`
 --
 ALTER TABLE `conditions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -282,6 +313,12 @@ ALTER TABLE `news`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
@@ -297,7 +334,23 @@ ALTER TABLE `registers`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `registers`
+--
+ALTER TABLE `registers`
+  ADD CONSTRAINT `registers_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
